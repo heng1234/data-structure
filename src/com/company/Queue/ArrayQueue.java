ffef53e6771f2764616a1fdef37cf5e3057d2d82
heng <luoheng@yihuisoft.com>
2019-10-01 16:55:18 +0800

    d队列的创建与c使用测试

--- src/com/company/Queue/ArrayQueue.java
+++ src/com/company/Queue/ArrayQueue.java
@@ -1,10 +1,110 @@
 package com.company.Queue;
 
+import com.company.Array.Array;
+
 /**
  * ArrayQueue
- *
+ * 队列的实现
  * @author heng
  * @date 2019/10/1
  **/
-public class ArrayQueue {
+public class ArrayQueue<E> implements  Queue<E> {
+
+    /**
+     * 初始化容器
+     */
+   private Array<E> array;
+
+    /**
+     * 初始化大小
+     * @param capacity
+     */
+    public ArrayQueue(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    /**
+     * 没有传入则默认大小
+     */
+    public ArrayQueue(){
+        array = new Array<>();
+    }
+    /**
+     * 存入队列中
+     * @param e
+     */
+    @Override
+    public void enqueue(E e) {
+      array.addLast(e);
+    }
+
+    /**
+     * 从队列中取出一个元素  出队
+     * @return
+     */
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+    /**
+     * 拿到队首
+     * @return
+     */
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+    /**
+     * 大小
+     * @return
+     */
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+    /**
+     * 是否为空
+     * @return
+     */
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    /**
+     * 查看有多少容积
+     * @return
+     */
+    public int getCapacity(){
+        return  array.getCapacaity();
+    }
+
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Queue: front [");
+        for (int i = 0; i < array.getSize(); i++) {
+            builder.append(array.get(i));
+            if (i != array.getSize()-1){
+                builder.append(" , ");
+            }
+        }
+        builder.append("] tail");
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 2){
+               queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+
+
+    }
 }
--- src/com/company/Queue/Queue.java
+++ src/com/company/Queue/Queue.java
@@ -2,8 +2,38 @@ package com.company.Queue;
 
 /**
  * Queue
- *
+ * 队列接口
  * @author heng
  **/
-public interface Queue {
+public interface Queue<E>{
+    /**
+     * 存入队列中
+     * @param e
+     */
+    void enqueue(E e);
+
+    /**
+     * 从队列中取出一个元素  出队
+     * @return
+     */
+    E dequeue();
+
+    /**
+     * 拿到队首
+     * @return
+     */
+    E getFront();
+
+    /**
+     * 大小
+     * @return
+     */
+    int getSize();
+
+    /**
+     * 是否为空
+     * @return
+     */
+    boolean isEmpty();
+
 }
