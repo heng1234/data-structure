e15127c20d3a38dd263791249c2caf2fe4b59ddc
heng <luoheng@yihuisoft.com>
2019-10-02 19:41:32 +0800

    s循环队列O(n)普通队列与循环队列O(1)PS:循环队列存在扩容和缩容存在均摊的测试比较O(n)和O(1)的性能

--- src/com/company/Queue/Main.java
+++ src/com/company/Queue/Main.java
@@ -1,5 +1,7 @@
 package com.company.Queue;
 
+import java.util.Random;
+
 /**
  * Main
  * 循环队列测试
@@ -7,9 +9,27 @@ package com.company.Queue;
  * @date 2019/10/2
  **/
 public class Main {
+    //测试使用q运行opCount个enqueue和dequeue操作所需要的时间 单位:秒
+  private static  double testQueue(Queue<Integer> q,int opCount){
+      long startTime = System.nanoTime();
+      Random random = new Random();
+      for (int i = 0; i < opCount; i++) {
+        q.enqueue(random.nextInt(Integer.MAX_VALUE));
+      }
+      for (int i = 0; i < opCount; i++) {
+          q.dequeue();
+      }
+      long endTime = System.nanoTime();
+      return  (endTime - startTime) / 1000000000.0;
+  }
 
+    /**
+     * 3.266439419
+     * 0.013643077
+     * @param args
+     */
     public static void main(String[] args) {
-        LoopQueue<Integer> queue = new LoopQueue<>();
+     /*   LoopQueue<Integer> queue = new LoopQueue<>();
         for (int i = 0; i < 12; i++) {
             queue.enqueue(i);
             System.out.println(queue);
@@ -17,7 +37,16 @@ public class Main {
                 queue.dequeue();
                 System.out.println(queue);
             }
-        }
+        }*/
 
+     //测试均摊时间
+
+        int opCount = 1000000;
+        ArrayQueue<Integer> arrayQueue =  new ArrayQueue<Integer>();
+        LoopQueue<Integer> loopQueue =  new LoopQueue<Integer>();
+        double time1 =testQueue(arrayQueue,opCount);
+        double time2 =testQueue(loopQueue,opCount);
+        System.out.println(time1);
+        System.out.println(time2);
     }
 }
